b52d7862aefa1fa075622e9ba5e4a3d1b651cec5
pablo <pablo@casi.dk>
2023-12-14 19:19:06 +0100

    adding base solution

---
+++ Planday.Schedule.Infrastructure/Queries/GetAllShiftsQuery.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using Planday.Schedule.Infrastructure.Providers.Interfaces;
+using Planday.Schedule.Queries;
+
+namespace Planday.Schedule.Infrastructure.Queries
+{
+    public class GetAllShiftsQuery : IGetAllShiftsQuery
+    {
+        private readonly IConnectionStringProvider _connectionStringProvider;
+
+        public GetAllShiftsQuery(IConnectionStringProvider connectionStringProvider)
+        {
+            _connectionStringProvider = connectionStringProvider;
+        }
+    
+        public async Task<IReadOnlyCollection<Shift>> QueryAsync()
+        {
+            await using var sqlConnection = new SqliteConnection(_connectionStringProvider.GetConnectionString());
+
+            var shiftDtos = await sqlConnection.QueryAsync<ShiftDto>(Sql);
+
+            var shifts = shiftDtos.Select(x => 
+                new Shift(x.Id, x.EmployeeId, DateTime.Parse(x.Start), DateTime.Parse(x.End)));
+        
+            return shifts.ToList();
+        }
+
+        private const string Sql = @"SELECT Id, EmployeeId, Start, End FROM Shift;";
+    
+        private record ShiftDto(long Id, long? EmployeeId, string Start, string End);
+    }    
+}
+
