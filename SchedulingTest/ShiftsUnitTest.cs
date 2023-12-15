14b5b8b24ffce98229061739de975c1ce4421c17
pablo <pablo@casi.dk>
2023-12-15 10:32:53 +0100

    was added SchedullingTest Project

---
+++ SchedulingTest/ShiftsUnitTest.cs
@@ -0,0 +1,32 @@
+namespace SchedulingTest
+{
+    public class ShiftsUnitTest
+    {
+        [Fact]
+        public void GetAllShifts_ShouldReturnListReadOnly_True()
+        {
+
+        }
+
+        [Fact]
+        public void GetShiftById_ShouldReturnId1_True()
+        {
+
+        }
+        [Fact]
+        public void GetShiftById_ShouldReturnId1_False()
+        {
+
+        }
+        [Fact]
+        public void GetShiftById_ShouldReturnNotFound_True()
+        {
+
+        }
+        [Fact]
+        public void GetShiftById_BadRequest_True()
+        {
+
+        }
+    }
+}
