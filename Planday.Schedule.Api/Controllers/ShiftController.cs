788ff21d47e941b2aa561e1313aec7dc9d44ade2
pablo <pablo@casi.dk>
2023-12-15 10:27:06 +0100

    was added methods GetAllShifts & GetShiftById

--- Planday.Schedule.Api/Controllers/ShiftController.cs
+++ Planday.Schedule.Api/Controllers/ShiftController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Planday.Schedule.Queries;
 
 namespace Planday.Schedule.Api.Controllers
 {
@@ -6,9 +7,34 @@ namespace Planday.Schedule.Api.Controllers
     [Route("[controller]")]
     public class ShiftController : ControllerBase
     {
-        public ShiftController()
+        private readonly IShiftsQuery _connection;
+        public ShiftController(IShiftsQuery connection)
         {
+            _connection = connection;
         }
-    }    
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyCollection<Shift>>> GetAllShifts()
+        {
+            return Ok(await _connection.GetAllShifts());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Shift>> GetShiftById([FromRoute] long id)
+        {
+            if (id <= 0)
+            {
+                ModelState.AddModelError("id", "The 'id' must be greater than 0.");
+                return BadRequest(ModelState);
+            }
+
+            var entity = await _connection.GetShiftById(id);
+
+            if (entity == null)
+                return NotFound();
+
+            return Ok(entity);
+        }
+    }
 }
 
