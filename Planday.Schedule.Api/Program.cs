099e683796e5f281b61e3e44429034908f5d1aea
pablo <pablo@casi.dk>
2023-12-19 18:06:55 +0100

    adding appsetting.json manager in the Startup

--- Planday.Schedule.Api/Program.cs
+++ Planday.Schedule.Api/Program.cs
@@ -4,6 +4,7 @@ using Planday.Schedule.Infrastructure.Queries;
 using Planday.Schedule.Queries;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -12,6 +13,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IConnectionStringProvider>(new ConnectionStringProvider(builder.Configuration.GetConnectionString("Database")));
 builder.Services.AddScoped<IShiftsQuery, ShiftsQuery>();
+builder.Services.AddScoped<IEmployeeQuery, EmployeeQuery>();
+builder.Services.AddScoped<IAssignamentQuery, AssignamentQuery>();
 
 var app = builder.Build();
 
