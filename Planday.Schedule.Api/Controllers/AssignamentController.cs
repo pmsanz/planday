54ae3a5375b5c7062bac514fd87654c5c55f6c10
pablo <pablo@casi.dk>
2023-12-19 18:07:59 +0100

    Adding Assignament controller, interface and infrastructure files

---
+++ Planday.Schedule.Api/Controllers/AssignamentController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Planday.Schedule.Queries;
+
+namespace Planday.Schedule.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AssignamentController : ControllerBase
+    {
+
+        private readonly IAssignamentQuery _connectionAssignament;
+        private readonly IShiftsQuery _connectionShifts;
+        private readonly IEmployeeQuery _connectionEmployee;
+        public AssignamentController(IAssignamentQuery assignament, IEmployeeQuery employee, IShiftsQuery shift)
+        {
+            _connectionAssignament = assignament;
+            _connectionShifts = shift;
+            _connectionEmployee = employee;
+        }
+
+
+        [HttpPost("{shiftId}/{employeeId}")]
+        public async Task<ActionResult<Shift>> AddEmployeeToShift([FromRoute] long shiftId, [FromRoute] long employeeId)
+        {
+            if (shiftId < 0 || employeeId < 0)
+                return BadRequest("Ids must have a value grater than 0");
+
+            var employee = await _connectionEmployee.GetEmployeeById(employeeId);
+            var shift = await _connectionShifts.GetShiftById(shiftId);
+
+            if (employee == null)
+                return NotFound("Employee doesn't exists.");
+
+            if (shift == null)
+                return NotFound("Shift doesn't exists.");
+
+            var isAssigned = await _connectionShifts.IsAssigned(shift.Id);
+
+            if (isAssigned)
+                return BadRequest("Shift is already assigned.");
+
+            var isOverlapping = await _connectionEmployee.CheckOverlapping(shift, employeeId);
+
+            if (isOverlapping)
+                return BadRequest("Employee has an overlap with the given shift.");
+
+            await _connectionAssignament.AddEmployeeToShift(shiftId, employeeId);
+            var shiftUpdated = await _connectionShifts.GetShiftById(shiftId);
+            return Ok(shiftUpdated);
+
+        }
+
+
+
+    }
+}
+
+
---
+++ Planday.Schedule.Infrastructure/Queries/AssignamentQuery.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using Planday.Schedule.Infrastructure.Providers.Interfaces;
+using Planday.Schedule.Queries;
+
+namespace Planday.Schedule.Infrastructure.Queries
+{
+    public class AssignamentQuery : IAssignamentQuery
+    {
+        private readonly IConnectionStringProvider _connectionStringProvider;
+
+        private const string SqlUpdateShift = @"UPDATE Shift SET EmployeeId = {0} WHERE Id = {1};";
+        private record EmployeeDto(long Id, string Name);
+
+        public AssignamentQuery(IConnectionStringProvider connectionStringProvider)
+        {
+            _connectionStringProvider = connectionStringProvider;
+        }
+
+        public async Task AddEmployeeToShift(long shiftId, long employeeId)
+        {
+            if (shiftId < 1)
+                throw new Exception($"Shift should be greater than 0.");
+
+            if (employeeId < 1)
+                throw new Exception($"Employee should be greater than 0.");
+
+            await UpdateShiftInDatabase(shiftId, employeeId);
+        }
+
+        private async Task UpdateShiftInDatabase(long shiftId, long employeeId)
+        {
+            await using var sqlConnection = new SqliteConnection(_connectionStringProvider.GetConnectionString());
+            string updateSql = string.Format(SqlUpdateShift, employeeId, shiftId);
+            await sqlConnection.ExecuteAsync(updateSql);
+        }
+
+    }
+}
